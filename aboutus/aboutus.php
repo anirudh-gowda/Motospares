394ed588d95cb0201fb99122636d25b96b3475ac
anirudh-gowda <anirudhgowda05@gmail.com>
2024-03-10 21:00:30 +0530

    new update in adoutus

--- aboutus/aboutus.php
+++ aboutus/aboutus.php
@@ -20,25 +20,10 @@
         <img src="../images/team1.png" alt="image">
       </div>
       <h3>Anirudh HP</h3>
-      <p class="role">developer</p>
-      <p>Reg No: 18BC3004</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: anirudhgowda05@gmail.com</p></br>
+      <p class="role">developer / Tester</p>
+      <p>Email: anirudhgowda05@gmail.com</p></br>
     </div>&nbsp;&nbsp;
-    <div class="team_member">
-      <div class="team_img">
-        <img src="../images/team2.png" alt="image">
-      </div>
-      <h3>Avinash</h3>
-      <p class="role">Tester</p>
-      <p>Reg No: 18BC3009</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: avinash@gmail.com</p></br>
-    </div>&nbsp;&nbsp;
-    <div class="team_member">
-      <div class="team_img">
-        <img src="../images/team3.png" alt="image">
-      </div>
-      <h3>Dhanush</h3>
-      <p class="role">Support Lead</p>
-      <p>Reg No: 18BC3024</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: dhanush@gmail.com</p></br>
-    </div>
+
   </div>
 </div>	
 <a href="../login/index2.php" class="btn">Back</a>
