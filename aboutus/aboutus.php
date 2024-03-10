0334f8e8daa2b11c1b5f0b670fa28d00ea6a4e75
anirudh-gowda <anirudhgowda05@gmail.com>
2024-03-10 19:59:35 +0530

    new update

---
+++ aboutus/aboutus.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Our Team Section</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <div class="navbar">
+        <img src="../images/logo.png" class="logo">
+    </div>
+</div>
+
+<div class="wrapper">
+  <h1>Our Team</h1>
+  <div class="team">
+    <div class="team_member">
+      <div class="team_img">
+        <img src="../images/team1.png" alt="image">
+      </div>
+      <h3>Anirudh HP</h3>
+      <p class="role">developer</p>
+      <p>Reg No: 18BC3004</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: anirudhgowda05@gmail.com</p></br>
+    </div>&nbsp;&nbsp;
+    <div class="team_member">
+      <div class="team_img">
+        <img src="../images/team2.png" alt="image">
+      </div>
+      <h3>Avinash</h3>
+      <p class="role">Tester</p>
+      <p>Reg No: 18BC3009</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: avinash@gmail.com</p></br>
+    </div>&nbsp;&nbsp;
+    <div class="team_member">
+      <div class="team_img">
+        <img src="../images/team3.png" alt="image">
+      </div>
+      <h3>Dhanush</h3>
+      <p class="role">Support Lead</p>
+      <p>Reg No: 18BC3024</br>Qualification: BCA.</br>College: Government Science College,Hassan.</br>Email: dhanush@gmail.com</p></br>
+    </div>
+  </div>
+</div>	
+<a href="../login/index2.php" class="btn">Back</a>
+</body>
+</html>
